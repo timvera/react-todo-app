0607982b0478e81bf3abcdb1bea861916e9b1da0
Veramoses <etimveronica724@gmail.com>
2026-08-11 22:53:29 +0000

    feat: add todo input state

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 export default function TodoForm() {
+  const [task, setTask] = useState("");
+
+  function handleSubmit() {
+  setTask("");
+}
+
   return (
     <section className="todo-form">
       <input
         type="text"
         placeholder="What do you need to do today?"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
       />
 
-      <button>Add Task</button>
+      <button onClick={handleSubmit}>Add Task</button>
+
+      <p>{task}</p>
     </section>
   );
 }
